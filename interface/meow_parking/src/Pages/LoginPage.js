19d02f4829c5040b1f6566ee2cb0dddb73c5b7e4
Uranus28 <83357358+Uranus28@users.noreply.github.com>
2023-06-09 16:08:05 +0500

    Update LoginPage.js
    переделал по класс + новый переход по страницам

--- interface/meow_parking/src/Pages/LoginPage.js
+++ interface/meow_parking/src/Pages/LoginPage.js
@@ -1,36 +1,59 @@
-import React, {useEffect} from 'react'
-import { useNavigate } from 'react-router-dom'
+
+import React,{Component} from 'react'
 import Header from "../Components/Header";
-import { useState } from 'react'
+import axios from 'axios'
+import {withRouter} from '../Components/withRouter';
+
+class Login extends Component{
 
 
-export default function Login() {
-    //
-    const navigateTo=useNavigate()
-    //состояние ввода
-    const [formInput, setFormInput] = React.useState({
-        email: "",
-        password: "",
-    })
-    //обработчик форм
-    const handleForm = () => {
-        console.log(formInput)
-        setFormInput({
-            email: "",
-            password: "",
-        })
-        alert('A form was submitted: '+ formInput.email +" " +formInput.password);
-        fetch('http://localhost:3000/store-data', {
-        method: 'POST',
-            // We convert the React state to JSON and send it as the POST body
-        body: JSON.stringify({email: formInput.email, password:formInput.password})
-        }).then(response => response.json())
-        .then(data => setFormInput({ email: data.email, password:data.password}));
+    constructor(props){
+      super(props)
+      //для перехода по страницам
+      this.navigateToHome=this.navigateToHome.bind(this);
+      this.navigateToLogin=this.navigateToLogin.bind(this);
+      this.navigateToSignUp=this.navigateToSignUp.bind(this);
+      //
+      this.state={
+        email: '',
+        password: '',
+      }
     }
-
-  return (
-    //описание фона
-    <div className=" bg-green-200 relative flex flex-col justify-center min-h-screen overflow-hidden">
+  
+    navigateToHome()
+    {
+        this.props.navigate('/home')
+    } 
+    navigateToLogin()
+    {
+        this.props.navigate('/')
+    } 
+    navigateToSignUp()
+    {
+        this.props.navigate('/signup')
+    }
+  
+  
+    changeHandler=(e)=>{
+      this.setState({[e.target.name]:e.target.value})
+    }
+    submitHandler =e=>{
+      e.preventDefault()
+      console.log(this.state)
+      axios.post('https://jsonplaceholder.typicode.com/posts',this.state)
+          .then(response=>{
+              console.log(response)
+          })
+          .catch(error=>{
+              console.log(error)
+          })
+    }
+    render()
+    {
+      //состояние ввода
+      const{email,password}=this.state
+      return(
+        <div className=" bg-green-200 relative flex flex-col justify-center min-h-screen overflow-hidden">
         <Header />
         <script>/* описание окна авторизации*/</script>
         <div className="w-full p-6 m-auto bg-white rounded-md ring ring-2 ring-transparent lg:max-w-xl">
@@ -41,7 +64,7 @@ export default function Login() {
             <script>/* описание email */</script>
               <div className="mb-2">
                 <label
-                    for="email"
+                    htmlFor="email"
                     className="block text-sm font-montesserat text-gray-800"
                 >
                     Почта
@@ -49,35 +72,33 @@ export default function Login() {
                 <script>/*значение value для передачи введеных значений пароля и почты в useState*/</script>
                 <script>/*setFormInput формула для чтения данных с формы*/</script>
                 <input  
-                    onChange={(e)=>setFormInput({ ...formInput, email: e.target.value })}              
-                    value={formInput.email}
-                    type="email"
+                    onChange={this.changeHandler}              
+                    value={email}
+                    type="text"
+                    name="email"
                     className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
               <script>/* описание пароля*/</script>
               <div className="mb-2">
                 <label
-                    for="password"
+                    htmlFor="password"
                     className="block text-sm font-montesserat text-gray-800"
                 >
                     Пароль
                 </label>
                 <input
-                    onChange={(e)=>setFormInput({ ...formInput, password: e.target.value })}                              
-                    value={formInput.password}
+                    onChange={this.changeHandler}                              
+                    value={password}
                     type="password"
+                    name="password"
                     className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
-              <script>/* ненужная кнопка забыли пароль
-                 <a href="#" className="text-xs font-montesserat text-purple-600 hover:underline"
-                  > Забыли пароль?
-              </a>
-              */</script>
+              
               <script>/* описание кнопки авторизации*/</script>
               <div className="mt-6">
-                    <button onClick={()=>{handleForm()}} className="w-full px-4 py-2 font-montesserat tracking-wide text-black transition-colors duration-200 transform bg-blue-100 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                    <button type="submit" onClick={this.submitHandler} className="w-full px-4 py-2 font-montesserat tracking-wide text-black transition-colors duration-200 transform bg-blue-100 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
                         Авторизоваться
                     </button>
                 </div>
@@ -86,7 +107,7 @@ export default function Login() {
             <p className="font-montesserat">
                 Ещё нету аккаунта?
                 <b 
-                    onClick={() => navigateTo("/signup")} 
+                    onClick={this.navigateToSignUp} 
                     className="font-montesserat hover: underline cursor-pointer"
                 >
                     Зарегестрируйтесь
@@ -94,5 +115,8 @@ export default function Login() {
             </p>
         </div>
     </div>
-  )
+
+      )
+    }
 }
+export default withRouter(Login)
