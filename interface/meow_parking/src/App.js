edc718f6d1175c88c2f113ef0386058a980ed301
Uranus28 <83357358+Uranus28@users.noreply.github.com>
2023-06-27 00:01:23 +0500

    удалены ненужные маршруты

--- interface/meow_parking/src/App.js
+++ interface/meow_parking/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Login from './Pages/LoginPage'
 import SignUp from './Pages/SignUpPage'
 import Home from './Pages/Home';
-import PostForm from './Pages/PostForm';
+//import PostForm from './Pages/PostForm';
 //import MyForm from './Pages/MyForm';
 // <Route path='/MyForm' element={<MyForm />} />
 import Header from "./Components/Header";
@@ -21,7 +21,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/postform' element={<PostForm/>}/>
         <Route path='/home' element={<Home />} />
       </Routes>
     </BrowserRouter>
