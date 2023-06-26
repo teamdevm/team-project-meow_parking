9ae3bc13a9f87c9454b2f44ef59f337e48af59e7
Uranus28 <yurasannikovv@gmail.com>
2023-06-27 00:08:14 +0500

    Убрал ненужные файлы

--- interface/meow_parking/src/App.js
+++ interface/meow_parking/src/App.js
@@ -3,10 +3,6 @@ import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Login from './Pages/LoginPage'
 import SignUp from './Pages/SignUpPage'
 import Home from './Pages/Home';
-import PostForm from './Pages/PostForm';
-//import MyForm from './Pages/MyForm';
-// <Route path='/MyForm' element={<MyForm />} />
-import Header from "./Components/Header";
 //import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -21,7 +17,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/postform' element={<PostForm/>}/>
         <Route path='/home' element={<Home />} />
       </Routes>
     </BrowserRouter>
